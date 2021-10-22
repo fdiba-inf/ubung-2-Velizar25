40411c5c14d377e474ed8eb9fcd1d03978adc056
Velizar25 <>
2021-10-22 10:25:31 +0000

    Files updated

--- src/exercise2/Circle.java
+++ src/exercise2/Circle.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class Circle {
   
-  public static void main(String[] args){
-    Scanner input = new Scanner(System.in);
-    
-    
-    double r = input.nextDouble();
-    double pi = 3.14;
-    double C = 2*pi*r;
-    double A = pi*r*r;
-    
-    System.out.println("Circumference: " + C );
-    System.out.println("Area: " + A);
-    
+   public static void main(String[] args){
+     Scanner input = new Scanner(System.in);
+      
+      
+      double r = input.nextDouble();
+      double pi = 3.14;
+      double C = 2*pi*r;
+      double A = pi*r*r;
+      
+      System.out.println("Circumference: " + C );
+      System.out.println("Area: " + A); 
+      
   }
 }
