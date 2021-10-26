c9c55b7a5a27d2f55d0355870f0dc9ee14b9b2e1
Velizar25 <>
2021-10-26 09:38:10 +0000

    Files updated

--- src/exercise2/QuadraticEquation.java
+++ src/exercise2/QuadraticEquation.java
@@ -1,36 +1,47 @@
 package exercise2;
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class QuadraticEquation {
 
-   public static void main(String[] args) {
-     Scanner input = new Scanner(System.in); 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double c = input.nextDouble();
+        double x1;
+        double x2;
+        double x;
 
-     System.out.print("Enter a , b, c: ");
-     double a = input.nextDouble();
-     double b = input.nextDouble();
-     double c = input.nextDouble();
+        double D;
+        if (a != 0) {
+          D = b*b - 4.00*a*c;
+        if (D >= 0) 
+        {
+            x1 = (-b + Math.sqrt(D))/2*a;
+            x2 = (-b - Math.sqrt(D))/2*a;
+            System.out.println (x1 + ", " + x2);
+        } 
+        else 
+        {
+            System.out.println ( "Imaginary values");
+        }
+        }
 
-     if (a != 0) {
-       double d = b * b - 4 * a * c;
-       if (d >= 0) {
-         double x1 = (-1 + Math.sqrt(d)) / 2 * a;
-         double x2 = (-1 - Math.sqrt(d)) / 2 * a;
-         System.out.println(x1 + "," + x2);
-      } else {
-        System.out.println( "Imaginary values");
-      }
-   
-     }  else if (b != 0) {
-         double x = -c / b;
-        System.out.println(x);
-     } else if (c != 0) {
-        System.out.println("No values");
-     } else {
-        System.out.println("Many values");
-     }
+        else if (b != 0) 
+        {
+          x = (0 - c)/b;
+          System.out.println (x);
 
+        } 
+        else if (c != 0) 
+        {
+          System.out.println ( "No values");
+        } 
+        else if (c == 0)  
+        {
+          System.out.println ( "Many values");
+        }
   }
-
- }     
+}
